9c5009153e38b6dab88c1dc6d133febdc900af4d
Alvaroorta1 <165050201+Alvaroorta1@users.noreply.github.com>
2024-05-02 22:53:08 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  function generadorDeDominios(arr1, arr2, arr3) {
+    for (let idx1 of arr1) {
+      for (let idx2 of arr2) {
+        for (let idx3 of arr3) {
+          console.log(`${idx1}${idx2}${idx3}.com`);
+        }
+      }
+    }
+  }
+  generadorDeDominios(pronoun, adj, noun);
   console.log("Hello Rigo from the console!");
 };
